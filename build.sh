9f2bc23b8096b1f989eda1ae5c1e75cdf6a85ff6
PotatoAom <oomsin2301@hotmail.co.th>
2026-02-19 12:51:50 +0700

    v1.023

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+python3.12 -m venv venv
+source venv/bin/activate
+
 pip install -r requirements.txt
-python manage.py migrate --noinput
+
 python manage.py collectstatic --noinput --clear
+python manage.py migrate --noinput
